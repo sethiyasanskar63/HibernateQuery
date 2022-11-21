8e26c95b76de7f47643841941a3f474a91b5f12a
sethiyasanskar63 <101315790+sethiyasanskar63@users.noreply.github.com>
2022-11-21 22:40:34 +0530

    Add files via upload

---
+++ HibernateCRUD/src/entity/Teacher.java
@@ -0,0 +1,78 @@
+package entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="teacher")
+
+public class Teacher {
+	
+	@Id
+	@Column(name="id")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
+	private int id;
+	
+	@Column(name="f_name")
+	private String f_name;
+	
+	@Column(name="l_name")
+	private String l_name;
+	
+	@Column(name="email")
+	private String email;
+	
+	public Teacher() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+	
+	public Teacher(String f_name, String l_name, String email) {
+		super();
+		this.f_name = f_name;
+		this.l_name = l_name;
+		this.email = email;
+	}	
+	
+	public int getId() {
+		return id;
+	}
+	
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getF_name() {
+		return f_name;
+	}
+	
+	public void setF_name(String f_name) {
+		this.f_name = f_name;
+	}
+	
+	public String getL_name() {
+		return l_name;
+	}
+	
+	public void setL_name(String l_name) {
+		this.l_name = l_name;
+	}
+	
+	@Override
+	public String toString() {
+		return "Teacher [id=" + id + ", f_name=" + f_name + ", l_name=" + l_name + ", email=" + email + "]";
+	}
+
+	public String getEmail() {
+		return email;
+	}
+	
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+}
---
+++ HibernateCRUD/src/service/Test.java
@@ -0,0 +1,16 @@
+package service;
+
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		System.out.println("Connecting to DB");
+		SessionFactory factory =new Configuration()
+				.configure("hibernate.cfg.xml")
+				.buildSessionFactory();
+		System.out.println("Created");
+	}
+
+}
